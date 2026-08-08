b1aee21c99c94486df85274699f26703ebad9e64
hayyantahirr <hayyantahir7@gmail.com>
2026-08-09 02:12:32 +0500

    firebase added to the project

---
+++ config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBGSf45sY4H9Bmh6IBGzliQ67xRzZpJ5tc",
+  authDomain: "alinea-online-92d2b.firebaseapp.com",
+  projectId: "alinea-online-92d2b",
+  storageBucket: "alinea-online-92d2b.firebasestorage.app",
+  messagingSenderId: "256549555780",
+  appId: "1:256549555780:web:8e1ace2a6a99e1ee19b611"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
